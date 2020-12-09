7b537c88ddb9b3b10d7dff9f45bb9c9837350022
Jack Joseph <dunleavyjack@gmail.com>
2020-12-09 16:21:27 +0900

    Initial Commit

---
+++ CT-Covid-Data-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CT-Covid-Data-App
+//
+//  Created by Jack on 12/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
