e33ec5fd8279c7a85ddd534bb914246ef52b9935
Jack Joseph <dunleavyjack@gmail.com>
2020-12-15 22:39:48 +0900

    Reorganized and structured

--- CT-Covid-Data-App/ViewController.swift
+++ CT-Covid-Data-App/ViewController.swift
@@ -9,8 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    // Buttons go here
-    
+    // UI Labels
     @IBOutlet weak var percentPositiveLabel: UILabel!
     @IBOutlet weak var percentChangeLabel: UILabel!
     @IBOutlet weak var percentChangeTriangle: UIImageView!
@@ -21,74 +20,63 @@ class ViewController: UIViewController {
     @IBOutlet weak var testsLabel: UILabel!
     @IBOutlet weak var totalCasesLabel: UILabel!
     
+    // UI Buttons
     @IBAction func linkButton(_ sender: UIButton) {
         UIApplication.shared.open(URL(string: "https://portal.ct.gov/Coronavirus" )! as URL, options: [:], completionHandler: nil)
     }
-    // Buttons end here
         
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // Organize fetched JSON Data
         fetchTotalsData { (json) in
-            
             
             // Get and organize dates
             let today = json[0].date!
-            // Get month, day, and year
+            
+            // Get and organize date
             let dateYear = today.prefix(4);
             let dateMonth = today.dropFirst(5).prefix(2) as Substring
             let dateDay = today.dropFirst(8).prefix(2) as Substring
             let fixedDateYear = String(dateYear)
             let fixedDateMonth = String(dateMonth)
             let fixedDateDay = String(dateDay)
-            
-            
             let todayDate = fixedDateMonth + "/" + fixedDateDay + "/" + fixedDateYear
             
+            // Today's Basic Data
             let confirmedCases = String(Int(json[0].confirmedcases)! - Int(json[1].confirmedcases)!)
             let probableCases = String(Int(json[0].probablecases)! - Int(json[1].probablecases)!)
-            
-//            let todayReported = json[0].covid_19_tests_reported!
             let todayTotalCases = json[0].totalcases!
             let todayTotalTests = json[0].covid_19_tests_reported!
             
-            // Yesterday
-//            let yesterday = json[1].date!
-//            let yesterdayReported = json[1].covid_19_tests_reported!
+            // Yesterday's Basic Data
             let yesterdayTotalCases = json[1].totalcases!
             let yesterdayTotalTests = json[1].covid_19_tests_reported!
             
-            //Day Before Yesterday
-//            let beforeYesterday = json[2].date!
-//            let beforeYesterdayReported = json[2].covid_19_tests_reported!
+            // Day Before Yesterday's Basic Data
             let beforeYesterdayTotalCases = json[2].totalcases!
             let beforeYesterdayTotalTests = json[2].covid_19_tests_reported!
             
-            // today Tests and yesterday total cases
+            
+            // Today's Calculated New Tests and Cases
             let todaysTests = String(Int(todayTotalTests)! - Int(yesterdayTotalTests)!)
             let newCases = String(Int(todayTotalCases)! - Int(yesterdayTotalCases)!)
             
+            // Yesterday's Calculated Tests and Cases
             let yesterdayTests = String(Int(yesterdayTotalTests)! - Int(beforeYesterdayTotalTests)!)
             let yesterdayNewCases = String(Int(yesterdayTotalCases)! - Int(beforeYesterdayTotalCases)!)
             
-        
-            //begining of hard things
+            // Today and Yesterday's Percent
             let todayPercent = getPercentPositive(increasedTests: todaysTests, increasedCases: newCases)
             let yesterdayPercent = getPercentPositive(increasedTests: yesterdayTests, increasedCases: yesterdayNewCases)
-            print(todayPercent)
-            print(yesterdayPercent)
-            
-            
-            //Percent Difference always error
+
+            // Percent Difference Between Today and Yesterday
             let percentChange = getPercentChange(today: todayPercent, yesterday: yesterdayPercent)
             
-            
-            
-//            let percentChangeFixed = formatPercentChange(percent: percentChange)
-            
-            
+            // Execute and Pass Data to the UI
             DispatchQueue.main.async {
+                // Check for negative percent change
                 if percentChange.prefix(1) == "-"{
                     self.percentChangeTriangle.image = #imageLiteral(resourceName: "upsidedown")
                     self.percentChangeLabel.text = String(percentChange.dropFirst(1))
@@ -96,7 +84,7 @@ class ViewController: UIViewController {
                     self.percentChangeTriangle.image = #imageLiteral(resourceName: "Polygon 1")
                     self.percentChangeLabel.text = percentChange
                 }
-                
+                // UI Labels
                 self.percentPositiveLabel.text = todayPercent
                 self.dateLabel.text = todayDate
                 self.newCasesLabel.text = addCommas(numberString: newCases)
@@ -105,43 +93,15 @@ class ViewController: UIViewController {
                 self.testsLabel.text = addCommas(numberString: todaysTests)
                 self.totalCasesLabel.text = addCommas(numberString: todayTotalCases)
             }
-//
-//            print("############# Today ##################")
-//            print("Today's date: " + today)
-//            print("Cases reported today: " + todayReported)
-//            print("Total Cases: " + todayTotalCases)
-//            print("Year is " + fixedDateYear)
-//            print("Month is " + fixedDateMonth)
-//            print("Day is " + fixedDateDay)
-//            print(fixedDateMonth + "/" + fixedDateDay + "/" + fixedDateYear)
-//            print(todayPercent)
-//
-//            print("############# Changes ##################")
-//            print(newCases)
-//
-//            print("########### Yesterday  ###############")
-//            print("Yesterday's date: " + yesterday)
-//            print("Cases reported yesterday: " + yesterdayReported)
-//            print("Total Cases: " + yesterdayTotalCases)
-//
-//            print("########### Day Before Yesterday  ###############")
-//            print("Day before yesterday's date: " + beforeYesterday)
-//            print("Cases reported the day before yesterday: " + beforeYesterdayReported)
-//            print("Total Cases: " + beforeYesterdayTotalCases)
-//
-            
         }
     }
     
-    
+    // Fetch and parse JSON Data
     func fetchTotalsData(completionHandler: @escaping([Totals]) -> Void){
-        let url = URL(string: "https://data.ct.gov/resource/rf3k-f8fg.json")!
-        
-        // At the begining of the next line there used to be "task =" before the URLSession call but it showed an error so I removed it and things are still working
+        let url = URL(string: "https://data.ct.gov/resource/rf3k-f8fg.json?$$app_token=oqfaeHTIlak3SguVrHEfqIs4n")!
+        // Start URL Session
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            
             guard let data = data else { return }
-            
             do {
                 let totalsData = try JSONDecoder().decode([Totals].self, from: data)
             
